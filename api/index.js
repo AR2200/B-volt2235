34662bf4e90c910bafe4592a5186646a4c243f5d
AR2200 <anryo1216@gmail.com>
2022-03-22 23:00:41 +0900

    aaaaa

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3000; //Heroku用
 const bodyParser = require("body-parser");
 //app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
+/*app.get("/", (req, res) => {
   res.send("Hello World(*'▽')");
-});
+});*/
 app.get("/api/get/", (req, res) => {
   res.json({ message: "こちらはルートパスです(V)o￥o(V)." });
 });
